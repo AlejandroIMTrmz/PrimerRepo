f9be73e73cefc1b0b23558f2565ca6f37b3b7490
Alejandro Ramírez <alejandro_3091@live.com.mx>
2014-02-25 15:43:33 -0600

    Modificando el estado del documento

--- Hello_world.c
+++ Hello_world.c
@@ -5,5 +5,6 @@ int main()
     printf("Hello world!\n");
     printf("Primeros pasos git!\n");
     printf("Segunda modificacion, -a con git\n");
+    printf("Modificacion de estado")
     return 0; 
 }
