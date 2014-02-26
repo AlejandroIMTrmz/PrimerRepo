eac7ecd42c93fe2b44d6887c3d1f378835dfbeed
Alejandro Ramírez <alejandro_3091@live.com.mx>
2014-02-26 15:33:33 -0600

    Modificacion en la rama 3, correcion de suma.c

--- Codigos/suma.c
+++ Codigos/suma.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main()
 {
-    printf("x + y = z!'n");
+    printf("x + y = z!\n");
+    printf("Modificacion en la rama3_suma\n");
+    
+    system("pause");
     return 0;
 }
