d977e0ffa33cee19ee839213e5574fe935e18fdc
Alejandro Ramírez <alejandro_3091@live.com.mx>
2014-02-27 12:24:51 -0600

    Multiplicacion de dos numeros en la rama master

--- Codigos/Hello.c
+++ Codigos/Hello.c
@@ -3,9 +3,11 @@
 
 int main()
 {
+    //Multiplicacion de dos numeros en master
     int x=3, y=3, z;  
     printf("Hello!\n");
     
+    //Multiplicacion
     z = x * y; 
     
     system("Pause");
